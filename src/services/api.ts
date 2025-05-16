e95e98ddad1eb96ab50bd4660142c767ad26bd49
softmanu-tech <walelae86@gmail.com>
2025-05-16 03:28:42 -0700

    Auto-commit: Fri May 16 03:28:42 PDT 2025

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,3 +2,27 @@ const attendanceRecords = await Attendance.find(attendanceFilter).lean<Attendanc
 const members = await User.find({ group: leader.group._id, role: 'member' })
   .select('name email phone')
   .lean<Member[]>();
+
+
+
+
+
+
+
+
+
+
+  attendanceRecords.forEach((record) => {
+    record.presentMembers?.forEach((memberId) => {
+      const idStr = memberId.toString();
+      if (memberAttendanceMap.has(idStr)) {
+        const data = memberAttendanceMap.get(idStr)!;
+        data.count += 1;
+        if (!data.lastDate || record.date > data.lastDate) {
+          data.lastDate = record.date;
+        }
+        memberAttendanceMap.set(idStr, data);
+      }
+    });
+  });
+  
