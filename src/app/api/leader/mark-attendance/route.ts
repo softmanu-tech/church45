6cb33ed52579a3339212d4dfdebcc92b58a01b65
softmanu-tech <walelae86@gmail.com>
2025-05-20 13:06:23 -0700

    Tue May 20 13:06:23 PDT 2025

--- src/app/api/leader/mark-attendance/route.ts
+++ src/app/api/leader/mark-attendance/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import { requireSessionAndRoles } from "@/lib/authMiddleware";
-import Group from "@/lib/models/Group";
+import Group} from "@/lib/models/Group";
 import Event from "@/lib/models/Event";
 import Member from "@/lib/models/Member";
 import mongoose from "mongoose";
