eaddc4341e9e83b18e9848361658a72080ee6a6e
softmanu-tech <walelae86@gmail.com>
2025-05-20 03:31:10 -0700

    Auto-commit: Tue May 20 03:31:10 PDT 2025

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -3,3 +3,7 @@ export * from './User';
 export * from './Group';
 export * from './Attendance';
 // Add other model exports as needed
+// src/lib/models/index.ts
+export * from './Attendance';
+export * from './Group';
+export * from './User';
