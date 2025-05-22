24c45848a3446c8fc3e523cbbae6606c67393942
softmanu-tech <walelae86@gmail.com>
2025-05-22 12:18:23 -0700

    updates

---
+++ src/app/(dashboard)/leader/members/page.tsx
@@ -0,0 +1,17 @@
+MemberSchema.pre<IMember>("save", async function (next) {
+    if (!this.isModified("password")) return next()
+  
+    try {
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(this.password, salt)
+      next()
+    } catch (error) {
+      // Cast error to 'CallbackError' or handle unknown error safely
+      if (error instanceof Error) {
+        next(error)
+      } else {
+        next(new Error("Unknown error during password hashing"))
+      }
+    }
+  })
+  
