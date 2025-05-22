8874acb02e572e887629fa52e0742c8d002ed468
softmanu-tech <walelae86@gmail.com>
2025-05-22 05:32:45 -0700

    Thu May 22 05:32:45 PDT 2025

--- src/components/CreateMemberForm.tsx
+++ src/components/CreateMemberForm.tsx
@@ -124,7 +124,7 @@ export function CreateMemberForm({ groupId }: CreateMemberFormProps) {
         className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         required
     >
-        <option value="">Select a role</option>
+        <option value="">Select </option>
         <option value="member">Member</option>
         <option value="leader">Leader</option>
     </select>
