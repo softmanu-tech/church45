0c071e6eb0d41c5e82646269b4fa0ed8b00e5108
softmanu-tech <walelae86@gmail.com>
2025-05-22 10:34:15 -0700

    updates

--- src/lib/models/Member.ts
+++ src/lib/models/Member.ts
@@ -1,5 +1,5 @@
-// models/Member.ts
 import mongoose, { Schema, Document, Model } from "mongoose";
+import bcrypt from 'bcrypt';
 
 export interface IMember extends Document {
   name: string;
@@ -9,7 +9,7 @@ export interface IMember extends Document {
   location?: string;
   group: mongoose.Types.ObjectId; // ref to Group
   role: string;
-  password: string
+  password: string;
   createdAt: Date;
   updatedAt: Date;
   leader: mongoose.Types.ObjectId; // ref to User
@@ -18,17 +18,25 @@ export interface IMember extends Document {
 const MemberSchema: Schema<IMember> = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String},
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
     phone: { type: String },
     department: { type: String },
     location: { type: String },
     group: { type: Schema.Types.ObjectId, ref: "Group", required: true },
-    leader: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    leader: { type: Schema.Types.ObjectId, ref: "User ", required: true },
     role: { type: String, required: true, enum: ["member", "leader"] },
     password: { type: String, required: true },
   },
   { timestamps: true }
 );
+
+// Hash the password before saving
+MemberSchema.pre<IMember>('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
 const Member: Model<IMember> = mongoose.models.Member || mongoose.model<IMember>("Member", MemberSchema);
 export default Member;
