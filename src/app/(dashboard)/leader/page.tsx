850d548048cc79825247742afa87fdb7fc5ccb04
softmanu-tech <walelae86@gmail.com>
2025-05-23 07:38:02 -0700

    contxtFri May 23 07:38:02 PDT 2025

--- src/app/(dashboard)/leader/page.tsx
+++ src/app/(dashboard)/leader/page.tsx
@@ -439,7 +439,7 @@ export default function LeaderDashboard() {
       { openAddMember &&  (
         <CreateMemberForm
         groupId={data.group._id} 
-        leaderId={data.leader._id()}
+        leaderId={data.group.leader.()}
 
         onMemberCreated={() => {
           setOpenAddMember(false);
