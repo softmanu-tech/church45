44cb4b49c23abe1ba8c0cb1f29745f78fe94a0a0
softmanu-tech <walelae86@gmail.com>
2025-05-23 04:45:42 -0700

    updates

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -136,9 +136,7 @@ export function CreateMemberForm({ groupId, leaderId, onMemberCreated }: CreateM
           className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           required
         >
-          <option value="">Select </option>
           <option value="member">Member</option>
-          <option value="leader">Leader</option>
         </select>
       </div>
       <div>
