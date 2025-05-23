31f959e8b881e3ad3cf39fad1bb40301c184bac6
softmanu-tech <walelae86@gmail.com>
2025-05-23 07:20:38 -0700

    contxtFri May 23 07:20:38 PDT 2025

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,15 +2,17 @@
 import React, {useState} from "react";
 import {} from "@tanstack/query-core";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { UserProvider } from "@/context/UserContext";
 
 export default function Providers({children} : {children: React.ReactNode}) {
 
     const [queryClient] =  useState(() => new  QueryClient())
     return(
         <QueryClientProvider client={queryClient}>
-                <UserProvider><UserP
+                <UserProvider>
+                    {children}
+                </UserProvider>
 
-            {children}
         </QueryClientProvider>
     )
 }
