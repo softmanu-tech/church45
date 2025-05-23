62eb8944cf5108fa5fd80c4b8dab7f1fa5d29f54
softmanu-tech <walelae86@gmail.com>
2025-05-23 04:46:44 -0700

    updates

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -1,4 +1,6 @@
 "use client"
+
+import { on } from "events";
 import React, {  useState } from "react"
 import { toast } from "react-toastify"
 
@@ -136,7 +138,9 @@ export function CreateMemberForm({ groupId, leaderId, onMemberCreated }: CreateM
           className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           required
         >
+          <option value="">Select </option>
           <option value="member">Member</option>
+          <option value="leader">Leader</option>
         </select>
       </div>
       <div>
