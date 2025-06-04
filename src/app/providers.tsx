f4333592290cd7d183de9f3688be4db16d13aa52
softmanu-tech <walelae86@gmail.com>
2025-06-04 06:29:41 -0700

    commit: Wed Jun  4 06:29:41 PDT 2025

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,16 +2,13 @@
 import React, {useState} from "react";
 import {} from "@tanstack/query-core";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { UserProvider } from "@/context/UserContext";
 
 export default function Providers({children} : {children: React.ReactNode}) {
 
     const [queryClient] =  useState(() => new  QueryClient())
     return(
         <QueryClientProvider client={queryClient}>
-                <UserProvider>
                     {children}
-                </UserProvider>
 
         </QueryClientProvider>
     )
